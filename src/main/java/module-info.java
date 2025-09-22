44e6bbfe96b4818d00eb271db4ec6db8b82afdeb
Aswath Chandrasekaran <92445737+Aswath1999@users.noreply.github.com>
2025-09-22 18:45:44 +0530

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,22 +3,7 @@ module Advanced_Java_for_Bioinformatics {
     requires javafx.graphics;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
-    exports assignment1.anatomy;
-    exports assignment02.anatomy;
-    opens assignment02.anatomy;
-
-    opens assignment03.window to javafx.fxml;
-    exports assignment03;
-
-    opens assignment4.window to javafx.fxml;
-    exports assignment4;
-
-    opens assignment5.window to javafx.fxml;
-    exports assignment5;
-
-    opens assignment6.window to javafx.fxml;
-    exports assignment6;
-
+    
     opens HumanAnatomyViewer.window to javafx.fxml;
     exports HumanAnatomyViewer;
     opens HumanAnatomyViewer.model to javafx.fxml;
